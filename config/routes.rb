e1bf78d789126d385d7fa55a5fa3303c30ecfe0a
adnanolarmmi <olarmmi@gmail.com>
2024-01-20 12:47:40 +0100

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
+  get '/pages', to: 'pages#index'
+  #get request for the /pages path should go to the pages controller's index method
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
