14fe767db2c22dadfb26f95bfc5a38ce140ef00a
adnanolarmmi <olarmmi@gmail.com>
2024-01-25 18:26:34 +0100

    Delete comment functionality

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,46 @@
+class CommentsController < ApplicationController
+    before_action :set_post
+    def create
+       @comment = @post.comments.create(comment_params)
+       redirect_to @post
+    end
+
+    def edit 
+        @comment = Comment.find(params[:id])
+    end
+
+    def update
+        @comment = Comment.find(params[:id])
+        
+        respond_to do |format|
+          if @comment.update(comment_params)
+            format.html { redirect_to post_url(@post), notice: "Comment was successfully updated." }
+            format.json { render :show, status: :ok, location: @post }
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+            format.json { render json: @post.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+        # @page = Page.find(params[:id])
+        # @page.destroy
+    
+        respond_to do |format|
+          format.html { redirect_to post_url(@post), notice: "Comment was successfully destroyed." }
+          format.json { head :no_content }
+        end
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:name, :content)
+    end
+
+    def set_post
+        @post = Post.find(params[:post_id])
+    end
+end
